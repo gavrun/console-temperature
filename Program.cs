7b79aa3667983c686de084b8ac6c391175b3fba4
gavrun <38539655+gavrun@users.noreply.github.com>
2021-10-16 20:04:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_temperature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Convert temperature from F to C");
+        }
+
+        static double FahrenheitToCelsius(double temperatureFahrenheit)
+        {
+            double temperatureCelsius = (temperatureFahrenheit - 32) / 1.8;
+            return temperatureCelsius;
+        }
+    }
+}
